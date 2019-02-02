3ae5021e764d39dd4d3f61eb4da4208a23f70593
gemblok <47270198+gemblok@users.noreply.github.com>
2019-02-02 17:41:20 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// configurasi //
+
+
+// user_name boleh kosong //
+$user_name="";
+
+//user_gaid wajib isi
+$user_gaid="xxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+//user_token wajib di isi //
+$user_token="xxxxxxxxxxxx";
+
+//user_id wajib di isi //
+$user_id="Facebook_xxxxxxxxxxxxx";
+
+// invite_code wajib di isi //
+$invite_code="xxxxxxxxx";
+
+// Bearer wajib di isi //
+$Bearer="eyxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+
+
+
+?>
